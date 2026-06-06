c278008530e079f0ae9430f50b862c814505d6d9
Claire.Tsai <claire.tsai1986@gmail.com>
2026-06-05 22:54:29 -0700

    Initial Spring boot project

---
+++ src/test/java/com/claire/paymentledgerplatform/PaymentLedgerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claire.paymentledgerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentLedgerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
